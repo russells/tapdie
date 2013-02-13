2a43b356a7fbf89a3f6d930d2af2967145f23192
Russell Steicke <russells@adelie.cx>
2013-02-13 21:38:43 +0800

    t/20130212-led-display-at-startup (9/9 commits) 20130213-213843.526025230 burns

--- displays.c
+++ displays.c
@@ -8,24 +8,24 @@ Q_DEFINE_THIS_FILE;
 struct SevenSegmentDisplay displays[2];
 
 
-static struct SegmentMap {
-	char ch;
-	uint8_t segments;
-} segmentmap[] = {
-	{ '0', 0b01111110 },
-	{ '1', 0b00110000 },
+static uint8_t segmentmap[] = {
+	0b01111110,		/* '0' */
+	0b00110000,		/* '1' */
+	0b01101101,		/* '2' */
+	0b01111001,		/* '3' */
+	0b01111001,		/* '4' */
+	0b01011011,		/* '5' */
+	0b01011111,		/* '6' */
+	0b01110000,		/* '7' */
+	0b01111111,		/* '8' */
+	0b01111011,		/* '9' */
 };
 
 static uint8_t get_segmentmap(char ch)
 {
-	struct SegmentMap *smp = segmentmap;
-	while (smp->ch) {
-		if (ch == smp->ch)
-			return smp->segments;
-		smp++;
-	}
-	Q_ASSERT(0);
-	return 0;
+	Q_ASSERT(ch >= '0');
+	Q_ASSERT(ch <= '9');
+	return segmentmap[ ch - '0' ];
 }
 
 
@@ -42,7 +42,8 @@ void displays_init(void)
 
 void set_digit(uint8_t digit, char ch, uint8_t brightness)
 {
-	uint8_t segments = get_segmentmap(ch);
+	uint8_t segments = get_segmentmap(ch & 0x7f);
+	if (ch & 0x80) segments |= 0x80;
 	switch (digit) {
 	case 0:
 		QF_INT_LOCK();
@@ -61,4 +62,11 @@ void set_digit(uint8_t digit, char ch, uint8_t brightness)
 	default:
 		Q_ASSERT(0);
 	}
+}
+
+
+void set_digits(char ch0, uint8_t br0, char ch1, uint8_t br1)
+{
+	set_digit(0, ch0, br0);
+	set_digit(1, ch1, br1);
 }

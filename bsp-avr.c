1aea17a818b47a31197b5326b41edd701cc1e318
Russell Steicke <russells@adelie.cx>
2013-04-18 13:03:25 +0800

    TopGit branch t/20130415-common-anode-or-cathode annihilated.

--- bsp-avr.c
+++ bsp-avr.c
@@ -3,7 +3,7 @@
 #include "morse.h"
 #include "displays.h"
 #include <avr/wdt.h>
-#include "common-diode.h"
+
 
 Q_DEFINE_THIS_FILE;
 
@@ -56,19 +56,16 @@ void BSP_startmain(void)
 }
 
 
-#ifdef COMMON_ANODE
-#define COM0xn 0b10	      /* Clear OC0A on compare match, set on BOTTOM. */
-#else
-#ifdef COMMON_CATHODE
-#define COM0xn 0b11	      /* Set OC0B on compare match, clear on BOTTOM. */
-#endif
-#endif
 
-
+/**
+ * @todo Make this sensitive to COMMON_ANODE and COMMON_CATHODE.  The polarity
+ * of the OCRA and OCRB pins will need to change (hence the COM0An and COM0Bn
+ * bits.)
+ */
 const uint8_t tccr0a_init =
-	(COM0xn << COM0A0) |
-	(COM0xn << COM0B0) |
-	(0b11 << WGM00);	/* Fast PWM. */
+	(0b10 << COM0A0) |    /* Clear OC0A on compare match, set on BOTTOM. */
+	(0b10 << COM0B0) |    /* Clear OC0B on compare match, set on BOTTOM. */
+	(0b11 << WGM00);      /* Fast PWM. */
 
 const uint8_t tccr0b_init =
 	(0 << WGM02) |		/* Fast PWM. */

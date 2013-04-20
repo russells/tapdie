2f31aaf8344a7be71a2b221bd0a86fbf87d46f41
Russell Steicke <russells@adelie.cx>
2013-04-20 12:58:52 +0800

    t/20130420-add-dashboard (2/2 commits) 20130420-125852.284875127 rlt4

--- dashboard.c
+++ dashboard.c
@@ -4,6 +4,8 @@
 
 static QState dashboardInitial(struct Dashboard *me);
 static QState dashboardState(struct Dashboard *me);
+static QState offState(struct Dashboard *me);
+static QState onState(struct Dashboard *me);
 
 
 struct Dashboard dashboard;
@@ -17,11 +19,37 @@ void dashboard_ctor(void)
 
 static QState dashboardInitial(struct Dashboard *me)
 {
-	return Q_TRAN(&dashboardState);
+	return Q_TRAN(&offState);
 }
 
 
 static QState dashboardState(struct Dashboard *me)
 {
 	return Q_SUPER(&QHsm_top);
+}
+
+
+static QState offState(struct Dashboard *me)
+{
+	switch (Q_SIG(me)) {
+	case DASH_ON_SIGNAL:
+		return Q_TRAN(onState);
+	case Q_ENTRY_SIG:
+		/* FIXME turn off the displays. */
+		break;
+	case Q_EXIT_SIG:
+		/* FIXME turn on the displays. */
+		break;
+	}
+	return Q_SUPER(&dashboardState);
+}
+
+
+static QState onState(struct Dashboard *me)
+{
+	switch (Q_SIG(me)) {
+	case DASH_OFF_SIGNAL:
+		return Q_TRAN(offState);
+	}
+	return Q_SUPER(&dashboardState);
 }

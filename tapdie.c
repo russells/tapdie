f4fef50e09105f73797fe24360b1faf353c7b1fc
Russell Steicke <russells@adelie.cx>
2013-04-24 20:05:04 +0800

    t/20130423-tap-to-change-mode (7/7 commits) 20130424-200504.898350178 rlt4

--- tapdie.c
+++ tapdie.c
@@ -215,7 +215,6 @@ static QState tappedState(struct Tapdie *me)
 	switch (Q_SIG(me)) {
 	case Q_ENTRY_SIG:
 		post(&dashboard, DASH_BRIGHTNESS_SIGNAL, 200);
-		rotate_mode(me);
 		display_mode(me);
 		QActive_arm((QActive*)me, (3 * BSP_TICKS_PER_SECOND) / 2);
 		return Q_HANDLED();
